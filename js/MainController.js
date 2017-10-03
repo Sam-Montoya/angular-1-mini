c62fac043f3722c59230b4b4c3030de13021a334
Brettly <brettly@Brettlys-MacBook-Pro.local>
2017-10-03 13:40:25 -0700

    Finished.

--- js/MainController.js
+++ js/MainController.js
@@ -1,8 +1,12 @@
-angular.module('friendsList').controller('MainController', function($scope) {
+angular.module('friendsList').controller('MainController', function ($scope) {
     $scope.friends = ['Brettly', 'Wheatly', 'Sam'];
 
-    $scope.addFriend = function(name) {
-        $scope.friends.push(name);
-        $scope.newFriend = '';
+    $scope.addFriend = function (name) {
+        if (name[0] !== name[0].toUpperCase()) {
+            $scope.friends.push(name[0].toUpperCase() + name.substr(1));
+        } else {
+            $scope.friends.push(name);
+            $scope.newFriend = '';
+        }
     }
 });
